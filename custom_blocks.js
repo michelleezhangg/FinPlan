6aeac256466f7081a892cd90bbfb418c9080112b
Cece Hagen <114951762+chagen7@users.noreply.github.com>
2023-10-01 19:08:58 -0700

    Update custom_blocks.js with first four blocks

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,1 +1,66 @@
-c
+Blockly.Blocks['income_source'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Income Source");
+    this.appendValueInput("INCOME_AMOUNT")
+        .setCheck(["Number", "var", "exp"])
+        .appendField("Amount");
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['expense'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Expense");
+    this.appendValueInput("EXPENSE_AMOUNT")
+        .setCheck(["Number", "var", "exp"])
+        .appendField("Amount");
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(330);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['savings'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Savings");
+    this.appendValueInput("SAVINGS_AMOUNT")
+        .setCheck(["Number", "var", "exp"])
+        .appendField("Amount");
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(120);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['budget'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Budget");
+    this.appendValueInput("INCOME")
+        .setCheck("exp")
+        .appendField("Income");
+    this.appendValueInput("EXPENSES")
+        .setCheck("exp")
+        .appendField("Expenses");
+    this.appendValueInput("SAVINGS")
+        .setCheck("exp")
+        .appendField("Savings");
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
