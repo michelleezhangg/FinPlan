f7f286466406d0b422e1abce8d7902447cf1f1fc
Cece Hagen <114951762+chagen7@users.noreply.github.com>
2023-10-01 20:01:05 -0700

    Update custom_blocks.js

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -1,66 +1,98 @@
+
+// Income Source Block
 Blockly.Blocks['income_source'] = {
   init: function() {
-    this.appendDummyInput()
+    this.appendValueInput("AMOUNT")
+        .setCheck("Number")
         .appendField("Income Source");
-    this.appendValueInput("INCOME_AMOUNT")
-        .setCheck(["Number", "var", "exp"])
-        .appendField("Amount");
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
+    this.setOutput(true, "Number");
     this.setColour(230);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
 
+Blockly.JavaScript['income_source'] = function(block) {
+  var amount = Blockly.JavaScript.valueToCode(block, 'AMOUNT', Blockly.JavaScript.ORDER_NONE);
+  return [amount, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+// Expense Block
 Blockly.Blocks['expense'] = {
   init: function() {
-    this.appendDummyInput()
+    this.appendValueInput("AMOUNT")
+        .setCheck("Number")
         .appendField("Expense");
-    this.appendValueInput("EXPENSE_AMOUNT")
-        .setCheck(["Number", "var", "exp"])
-        .appendField("Amount");
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(330);
+    this.setOutput(true, "Number");
+    this.setColour(0);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
 
+Blockly.JavaScript['expense'] = function(block) {
+  var amount = Blockly.JavaScript.valueToCode(block, 'AMOUNT', Blockly.JavaScript.ORDER_NONE);
+  return [amount, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+// Savings Block
 Blockly.Blocks['savings'] = {
   init: function() {
-    this.appendDummyInput()
+    this.appendValueInput("AMOUNT")
+        .setCheck("Number")
         .appendField("Savings");
-    this.appendValueInput("SAVINGS_AMOUNT")
-        .setCheck(["Number", "var", "exp"])
-        .appendField("Amount");
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(120);
+    this.setOutput(true, "Number");
+    this.setColour(110);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
 
+Blockly.JavaScript['savings'] = function(block) {
+  var amount = Blockly.JavaScript.valueToCode(block, 'AMOUNT', Blockly.JavaScript.ORDER_NONE);
+  return [amount, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+// Budget Block
 Blockly.Blocks['budget'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Budget");
     this.appendValueInput("INCOME")
-        .setCheck("exp")
-        .appendField("Income");
+        .setCheck("Number")
+        .appendField("Budget - Income");
     this.appendValueInput("EXPENSES")
-        .setCheck("exp")
+        .setCheck("Number")
         .appendField("Expenses");
     this.appendValueInput("SAVINGS")
-        .setCheck("exp")
+        .setCheck("Number")
         .appendField("Savings");
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(60);
+    this.setOutput(true, "Number");
+    this.setColour(290);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
 
+Blockly.JavaScript['budget'] = function(block) {
+  var income = Blockly.JavaScript.valueToCode(block, 'INCOME', Blockly.JavaScript.ORDER_NONE);
+  var expenses = Blockly.JavaScript.valueToCode(block, 'EXPENSES', Blockly.JavaScript.ORDER_NONE);
+  var savings = Blockly.JavaScript.valueToCode(block, 'SAVINGS', Blockly.JavaScript.ORDER_NONE);
+  var budget = '(' + income + ' - ' + expenses + ' - ' + savings + ')';
+  return [budget, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+// Input Number Block
+Blockly.Blocks['input_number'] = {
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.MATH_NUMBER_HELPURL);
+    this.setColour(Blockly.Msg.MATH_HUE);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber(0), 'NUM');
+    this.setOutput(true, 'Number');
+    this.setTooltip(Blockly.Msg.MATH_NUMBER_TOOLTIP);
+  }
+};
+
+Blockly.JavaScript['input_number'] = function(block) {
+  var number = block.getFieldValue('NUM');
+  return [number, Blockly.JavaScript.ORDER_ATOMIC];
+};
