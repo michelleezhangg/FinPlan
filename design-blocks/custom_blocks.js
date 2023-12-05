0aeffe1996ebb7c3a1a0e550547ebdc79ce332d9
Briana C <89816537+brcraig@users.noreply.github.com>
2023-12-04 19:37:29 -0800

    Update custom_blocks.js

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -1,4 +1,4 @@
-// Define the 'income_block'
+/*// Define the 'income_block'
 Blockly.Blocks['income_block'] = {
   init: function() {
     this.appendDummyInput()
@@ -111,10 +111,10 @@ function updateExpenseDisplay() {
 
 function updateBudgetDisplay(netBudget) {
   document.getElementById('budgetDisplay').textContent = `Net Budget: ${netBudget}`;
-}
+}*/
 
 
-/*
+
 // Income Source Block
 Blockly.Blocks['income_source'] = {
   init: function() {
@@ -211,4 +211,4 @@ Blockly.Blocks['input_number'] = {
 Blockly.JavaScript['input_number'] = function(block) {
   var number = block.getFieldValue('NUM');
   return [number, Blockly.JavaScript.ORDER_ATOMIC];
-};*/
+};
